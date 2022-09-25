6662e8de5cc2ae17fbd7381cc43d5779b619fa49
malev.dg <malev.dg@outlook.com>
2022-09-25 17:21:09 -0500

    css view sign in DONE

--- note-pad/src/components/login/Login.js
+++ note-pad/src/components/login/Login.js
@@ -3,31 +3,33 @@ import { FcGoogle } from "react-icons/fc";
 import style from './Login.module.css';
 import Logo from '../../imagenes/logo_notepad_white.png';
 import ImageLogin from '../../imagenes/image_login.png';
-import ImageBackground from '../../imagenes/bg_notepad.png';
+import CircleContainer from '../../imagenes/principal-circle.png';
 
 function Login() {
   return(
     <section className={style.loginContainer}>
         <div className= {style.imageLoginContainer} > 
-            <img 
-              className={style.imageLogin}
-              src= {ImageLogin}
-              alt='ImageLogo' />
+              <img 
+                  className={style.imageLogin}
+                  src= {ImageLogin}
+                  alt='ImageLogo'
+              />
         </div>
-        <form className={style.formContainer}>            
-            <img
-              className={style.logo}
-              src= {Logo}
-              alt='Logo' />                     
+        <form className={style.formContainer}>          
+              <img
+                  className={style.logo}
+                  src= {Logo}
+                  alt='Logo'
+              />                     
             <button type='submit' className={style.submitBtn}>
-					    Ingresar
+					       Ingresar
 				    </button>   
             <button className={style.googleSubmitBtn}>
                   <FcGoogle className={style.googleIcon} /> Continua con Google
 				    </button>            
-            <p className={style.questionSignUp} >¿No tienes una cuenta? Crea una aquí</p>                      
-        </form>
-    </section>
+            <p className={style.questionSignUp} >¿No tienes una cuenta? <b className={style.pathQuestionSignUp} >Crea una aquí</b></p>
+        </form>    
+    </section>  
   );
 }
 
